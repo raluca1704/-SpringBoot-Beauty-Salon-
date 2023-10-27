6e219cfcffbded4f76daa6bb03433bf7bd7e5d4c
RALUCx2 <raluca.chindris17@gmail.com>
2023-10-27 16:42:05 +0300

    clasa Client.java

---
+++ src/Client.java
@@ -0,0 +1,44 @@
+public class Client {
+    private int id;
+    private String email;
+    private String name;
+    private String telephoneNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
