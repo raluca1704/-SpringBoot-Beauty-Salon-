5dc4f142fc9b874dda34bb24f8357da475f42fee
RALUCx2 <raluca.chindris17@gmail.com>
2023-11-28 14:47:10 +0200

    modicat primul repo,emmployee

--- src/Main.java
+++ src/Main.java
@@ -13,42 +13,18 @@ import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
-        ServiceRepository serviceRepository = new ServiceRepository();
-        ServiceController serviceController = new ServiceController(serviceRepository);
-        LoyaltyCardRepository loyaltyCardRepository = new LoyaltyCardRepository();
-        LoyaltyCardController loyaltyCardController = new LoyaltyCardController(loyaltyCardRepository);
-        ClientRepository clientRepository = new ClientRepository();
-        ClientController clientController = new ClientController(clientRepository);
-
-        FeedbackRepository feedbackRepository = new FeedbackRepository();
-        FeedbackController feedbackController = new FeedbackController(feedbackRepository);
+//        ServiceRepository serviceRepository = new ServiceRepository();
+//        ServiceController serviceController = new ServiceController(serviceRepository);
+//        LoyaltyCardRepository loyaltyCardRepository = new LoyaltyCardRepository();
+//        LoyaltyCardController loyaltyCardController = new LoyaltyCardController(loyaltyCardRepository);
+//        ClientRepository clientRepository = new ClientRepository();
+//        ClientController clientController = new ClientController(clientRepository);
+//
+//        FeedbackRepository feedbackRepository = new FeedbackRepository();
+//        FeedbackController feedbackController = new FeedbackController(feedbackRepository);
 
         EmployeeRepository employeeRepository = new EmployeeRepository();
         EmployeeController employeeController = new EmployeeController(employeeRepository);
-
-        AppointmentRepository appointmentRepository = new AppointmentRepository();
-        AppointmentController appointmentController = new AppointmentController(appointmentRepository);
-
-        // Replace direct Service instantiation with factory.ServiceFactory
-        serviceController.addService(ServiceFactory.createService(1, "Hairstyling", 200));
-        serviceController.addService(ServiceFactory.createService(2, "Haircoloring", 70));
-        serviceController.addService(ServiceFactory.createService(3, "Haircutting", 150));
-        serviceController.addService(ServiceFactory.createService(5, "Semipemanent Manicure", 100));
-        serviceController.addService(ServiceFactory.createService(6, "Bridal Makeup", 300));
-        serviceController.addService(ServiceFactory.createService(7, "Evening Makeup", 150));
-        serviceController.addService(ServiceFactory.createService(8, "Fashion Makeup", 250));
-        serviceController.addService(ServiceFactory.createService(9, "Theatrical Makeup", 200));
-        serviceController.addService(ServiceFactory.createService(10, "Special Effects Makeup", 350));
-        serviceController.addService(ServiceFactory.createService(11, "Facial Treatment", 150));
-        serviceController.addService(ServiceFactory.createService(12, "Eyebrow Styling", 50));
-        serviceController.addService(ServiceFactory.createService(13, "Skin Cleansing", 120));
-        serviceController.addService(ServiceFactory.createService(14, "Anti-Aging Treatment", 200));
-        serviceController.addService(ServiceFactory.createService(15, "Semipemanent Pedicure", 100));
-        serviceController.addService(ServiceFactory.createService(16, "Manicure Construction", 180));
-        serviceController.addService(ServiceFactory.createService(17, "Pedicure Construction", 180));
-        serviceController.addService(ServiceFactory.createService(18, "Manicure Maintenance", 100));
-        serviceController.addService(ServiceFactory.createService(19, "Pedicure Maintenance", 100));
-
 
         Employee employee1 = new Employee.Builder()
                 .employeeID(1001)
@@ -59,85 +35,122 @@ public class Main {
                 .workStartTime(LocalTime.of(9, 0))
                 .workEndTime(LocalTime.of(16, 0))
                 .build();
-
-        Employee employee2 = new Employee.Builder()
-                .employeeID(1002)
-                .serviceID(2)
-                .name("Alice Johnson")
-                .birthDate(LocalDate.of(1990, 5, 15))
-                .rating("8.90")
-                .workStartTime(LocalTime.of(16, 0))
-                .workEndTime(LocalTime.of(20, 0))
-                .build();
-
-        Employee employee3 = new Employee.Builder()
-                .employeeID(1003)
-                .serviceID(3)
-                .name("David Lee")
-                .birthDate(LocalDate.of(1982, 8, 20))
-                .rating("10")
-                .workStartTime(LocalTime.of(8, 0))
-                .workEndTime(LocalTime.of(14, 0))
-                .build();
-
-        Employee employee4 = new Employee.Builder()
-                .employeeID(1004)
-                .serviceID(4)
-                .name("Emily Davis")
-                .birthDate(LocalDate.of(1995, 3, 25))
-                .rating("9.56")
-                .workStartTime(LocalTime.of(14, 0))
-                .workEndTime(LocalTime.of(18, 0))
-                .build();
-
-        Employee employee5 = new Employee.Builder()
-                .employeeID(1005)
-                .serviceID(5)
-                .name("Johanna Smith")
-                .birthDate(LocalDate.of(1985, 4, 17))
-                .rating("9.70")
-                .workStartTime(LocalTime.of(7, 0))
-                .workEndTime(LocalTime.of(12, 0))
-                .build();
-
-        Employee employee6 = new Employee.Builder()
-                .employeeID(1006)
-                .serviceID(6)
-                .name("Marry Jhons")
-                .birthDate(LocalDate.of(1999, 6, 20))
-                .rating("8.70")
-                .workStartTime(LocalTime.of(13, 30))
-                .workEndTime(LocalTime.of(20, 30))
-                .build();
-
-        Employee employee7 = new Employee.Builder()
-                .employeeID(1007)
-                .serviceID(7)
-                .name("Clara Claris")
-                .birthDate(LocalDate.of(1993, 5, 3))
-                .rating("8.40")
-                .workStartTime(LocalTime.of(9, 0))
-                .workEndTime(LocalTime.of(16, 0))
-                .build();
-
-        Employee employee8 = new Employee.Builder()
-                .employeeID(1008)
-                .serviceID(8)
-                .name("Elen Rules")
-                .birthDate(LocalDate.of(1995, 1, 2))
-                .rating("9.83")
-                .workStartTime(LocalTime.of(10, 0))
-                .workEndTime(LocalTime.of(15, 0))
-                .build();
-
         employeeController.addEmployee(employee1);
-        employeeController.addEmployee(employee2);
-        employeeController.addEmployee(employee3);
-        employeeController.addEmployee(employee4);
-        employeeController.addEmployee(employee5);
-        employeeController.addEmployee(employee6);
-        employeeController.addEmployee(employee7);
-        employeeController.addEmployee(employee8);
+
+//        AppointmentRepository appointmentRepository = new AppointmentRepository();
+//        AppointmentController appointmentController = new AppointmentController(appointmentRepository);
+//
+//        // Replace direct Service instantiation with factory.ServiceFactory
+//        serviceController.addService(ServiceFactory.createService(1, "Hairstyling", 200));
+//        serviceController.addService(ServiceFactory.createService(2, "Haircoloring", 70));
+//        serviceController.addService(ServiceFactory.createService(3, "Haircutting", 150));
+//        serviceController.addService(ServiceFactory.createService(5, "Semipemanent Manicure", 100));
+//        serviceController.addService(ServiceFactory.createService(6, "Bridal Makeup", 300));
+//        serviceController.addService(ServiceFactory.createService(7, "Evening Makeup", 150));
+//        serviceController.addService(ServiceFactory.createService(8, "Fashion Makeup", 250));
+//        serviceController.addService(ServiceFactory.createService(9, "Theatrical Makeup", 200));
+//        serviceController.addService(ServiceFactory.createService(10, "Special Effects Makeup", 350));
+//        serviceController.addService(ServiceFactory.createService(11, "Facial Treatment", 150));
+//        serviceController.addService(ServiceFactory.createService(12, "Eyebrow Styling", 50));
+//        serviceController.addService(ServiceFactory.createService(13, "Skin Cleansing", 120));
+//        serviceController.addService(ServiceFactory.createService(14, "Anti-Aging Treatment", 200));
+//        serviceController.addService(ServiceFactory.createService(15, "Semipemanent Pedicure", 100));
+//        serviceController.addService(ServiceFactory.createService(16, "Manicure Construction", 180));
+//        serviceController.addService(ServiceFactory.createService(17, "Pedicure Construction", 180));
+//        serviceController.addService(ServiceFactory.createService(18, "Manicure Maintenance", 100));
+//        serviceController.addService(ServiceFactory.createService(19, "Pedicure Maintenance", 100));
+//
+//
+//        Employee employee1 = new Employee.Builder()
+//                .employeeID(1001)
+//                .serviceID(1)
+//                .name("John Smith")
+//                .birthDate(LocalDate.of(1985, 4, 10))
+//                .rating("9.81")
+//                .workStartTime(LocalTime.of(9, 0))
+//                .workEndTime(LocalTime.of(16, 0))
+//                .build();
+//
+//        Employee employee2 = new Employee.Builder()
+//                .employeeID(1002)
+//                .serviceID(2)
+//                .name("Alice Johnson")
+//                .birthDate(LocalDate.of(1990, 5, 15))
+//                .rating("8.90")
+//                .workStartTime(LocalTime.of(16, 0))
+//                .workEndTime(LocalTime.of(20, 0))
+//                .build();
+//
+//        Employee employee3 = new Employee.Builder()
+//                .employeeID(1003)
+//                .serviceID(3)
+//                .name("David Lee")
+//                .birthDate(LocalDate.of(1982, 8, 20))
+//                .rating("10")
+//                .workStartTime(LocalTime.of(8, 0))
+//                .workEndTime(LocalTime.of(14, 0))
+//                .build();
+//
+//        Employee employee4 = new Employee.Builder()
+//                .employeeID(1004)
+//                .serviceID(4)
+//                .name("Emily Davis")
+//                .birthDate(LocalDate.of(1995, 3, 25))
+//                .rating("9.56")
+//                .workStartTime(LocalTime.of(14, 0))
+//                .workEndTime(LocalTime.of(18, 0))
+//                .build();
+//
+//        Employee employee5 = new Employee.Builder()
+//                .employeeID(1005)
+//                .serviceID(5)
+//                .name("Johanna Smith")
+//                .birthDate(LocalDate.of(1985, 4, 17))
+//                .rating("9.70")
+//                .workStartTime(LocalTime.of(7, 0))
+//                .workEndTime(LocalTime.of(12, 0))
+//                .build();
+//
+//        Employee employee6 = new Employee.Builder()
+//                .employeeID(1006)
+//                .serviceID(6)
+//                .name("Marry Jhons")
+//                .birthDate(LocalDate.of(1999, 6, 20))
+//                .rating("8.70")
+//                .workStartTime(LocalTime.of(13, 30))
+//                .workEndTime(LocalTime.of(20, 30))
+//                .build();
+//
+//        Employee employee7 = new Employee.Builder()
+//                .employeeID(1007)
+//                .serviceID(7)
+//                .name("Clara Claris")
+//                .birthDate(LocalDate.of(1993, 5, 3))
+//                .rating("8.40")
+//                .workStartTime(LocalTime.of(9, 0))
+//                .workEndTime(LocalTime.of(16, 0))
+//                .build();
+//
+//        Employee employee8 = new Employee.Builder()
+//                .employeeID(1008)
+//                .serviceID(8)
+//                .name("Elen Rules")
+//                .birthDate(LocalDate.of(1995, 1, 2))
+//                .rating("9.83")
+//                .workStartTime(LocalTime.of(10, 0))
+//                .workEndTime(LocalTime.of(15, 0))
+//                .build();
+//
+//        employeeController.addEmployee(employee1);
+//        employeeController.addEmployee(employee2);
+//        employeeController.addEmployee(employee3);
+//        employeeController.addEmployee(employee4);
+//        employeeController.addEmployee(employee5);
+//        employeeController.addEmployee(employee6);
+//        employeeController.addEmployee(employee7);
+//        employeeController.addEmployee(employee8);
+
+
 
 //        // Create default entities
 //        Client defaultClient = new Client();
@@ -175,9 +188,9 @@ public class Main {
 //        appointmentController.addAppointment(defaultAppointment);
 
 
-        UI ui = UI.getInstance(feedbackController, clientController, loyaltyCardController, serviceController, employeeController, appointmentController);
+     //   UI ui = UI.getInstance(feedbackController, clientController, loyaltyCardController, serviceController, employeeController, appointmentController);
 
         // Start the ui.UI
-        ui.start();
+    //    ui.start();
     }
 }
--- src/repository/EmployeeRepository.java
+++ src/repository/EmployeeRepository.java
@@ -2,85 +2,179 @@ package repository;
 
 import databasemodell.Employee;
 
+import java.sql.*;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EmployeeRepository {
-    private List<Employee> employees;
+public class EmployeeRepository extends SqlServer {
+    private Connection connection;
 
-
-    public EmployeeRepository() {
-        this.employees = new ArrayList<>();
+    public Connection EmployeeRepository() {
+        this.connection = SqlServer.conection();
+        return connection;
     }
 
     public void addEmployee(Employee employee) {
-        employees.add(employee);
+        String sql = "INSERT INTO employees (name, birthDate, workStartTime, workEndTime, serviceID, rating) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, employee.getName());
+            stmt.setDate(2, Date.valueOf(employee.getBirthDate()));
+            stmt.setTime(3, Time.valueOf(employee.getWorkStartTime()));
+            stmt.setTime(4, Time.valueOf(employee.getWorkEndTime()));
+            stmt.setInt(5, employee.getServiceID());
+            stmt.setString(6, employee.getRating());
+
+            int rows = stmt.executeUpdate();
+            if (rows > 0) {
+                ResultSet generatedKeys = stmt.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    int generatedId = generatedKeys.getInt(1);
+                    employee = new Employee.Builder()
+                            .employeeID(generatedId)
+                            .name(employee.getName())
+                            .birthDate(employee.getBirthDate())
+                            .workStartTime(employee.getWorkStartTime())
+                            .workEndTime(employee.getWorkEndTime())
+                            .serviceID(employee.getServiceID())
+                            .rating(employee.getRating())
+                            .build();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
     }
 
     public Employee getEmployeeById(int employeeID) {
-        for (Employee employee : employees) {
-            if (employee.getEmployeeID() == employeeID) {
-                return employee;
+        String sql = "SELECT * FROM employees WHERE employeeID = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, employeeID);
+            ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                return extractEmployeeFromResultSet(resultSet);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return null; // databasemodell.Employee not found
+        return null;
     }
 
     public List<Employee> getAllEmployees() {
-        return new ArrayList<>(employees); // Return a copy to avoid modifying the internal list
+        List<Employee> allEmployees = new ArrayList<>();
+        String sql = "SELECT * FROM employees";
+        try (Statement stmt = connection.createStatement();
+             ResultSet resultSet = stmt.executeQuery(sql)) {
+            while (resultSet.next()) {
+                Employee employee = extractEmployeeFromResultSet(resultSet);
+                allEmployees.add(employee);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return allEmployees;
     }
 
     public void updateEmployee(Employee updatedEmployee) {
-        for (int i = 0; i < employees.size(); i++) {
-            if (employees.get(i).getEmployeeID() == updatedEmployee.getEmployeeID()) {
-                employees.set(i, updatedEmployee);
-                return;
-            }
+        String sql = "UPDATE employees SET name=?, birthDate=?, workStartTime=?, workEndTime=?, serviceID=?, rating=? " +
+                "WHERE employeeID=?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, updatedEmployee.getName());
+            stmt.setDate(2, Date.valueOf(updatedEmployee.getBirthDate()));
+            stmt.setTime(3, Time.valueOf(updatedEmployee.getWorkStartTime()));
+            stmt.setTime(4, Time.valueOf(updatedEmployee.getWorkEndTime()));
+            stmt.setInt(5, updatedEmployee.getServiceID());
+            stmt.setString(6, updatedEmployee.getRating());
+            stmt.setInt(7, updatedEmployee.getEmployeeID());
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception appropriately
         }
     }
 
     public void deleteEmployee(int employeeID) {
-        employees.removeIf(employee -> employee.getEmployeeID() == employeeID);
+        String sql = "DELETE FROM employees WHERE employeeID=?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, employeeID);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
     }
+
     public List<Employee> getEmployeesByName(String name) {
         List<Employee> matchingEmployees = new ArrayList<>();
-
-        for (Employee employee : employees) {
-            if (employee.getName().equalsIgnoreCase(name)) {
+        String sql = "SELECT * FROM employees WHERE name LIKE ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, "%" + name + "%");
+            ResultSet resultSet = stmt.executeQuery();
+            while (resultSet.next()) {
+                Employee employee = extractEmployeeFromResultSet(resultSet);
                 matchingEmployees.add(employee);
             }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception appropriately
         }
-
         return matchingEmployees;
     }
+
     public LocalTime getWorkStartTimeById(int employeeID) {
-        for (Employee employee : employees) {
-            if (employee.getEmployeeID() == employeeID) {
-                return employee.getWorkStartTime();
+        String sql = "SELECT workStartTime FROM employees WHERE employeeID=?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, employeeID);
+            ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getTime("workStartTime").toLocalTime();
             }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception appropriately
         }
         return null; // Employee not found
     }
+
     public LocalTime getWorkEndTimeById(int employeeID) {
-        for (Employee employee : employees) {
-            if (employee.getEmployeeID() == employeeID) {
-                return employee.getWorkEndTime();
+        String sql = "SELECT workEndTime FROM employees WHERE employeeID=?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, employeeID);
+            ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getTime("workEndTime").toLocalTime();
             }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception appropriately
         }
         return null; // Employee not found
     }
+
     public String getServiceNameById(int employeeID) {
-        for (Employee employee : employees) {
-            if (employee.getEmployeeID() == employeeID) {
-                int serviceID = employee.getServiceID();
-                // Assuming you have a service repository to get the service name by ID
-                ServiceRepository serviceRepository = null;
-                String serviceName = serviceRepository.getServiceById(serviceID).getName();
-                return serviceName;
+        String sql = "SELECT services.name FROM employees " +
+                "INNER JOIN services ON employees.serviceID = services.serviceID " +
+                "WHERE employees.employeeID = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, employeeID);
+            ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getString("name");
             }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception appropriately
         }
         return null; // Employee not found or associated service not found
     }
 
+    private Employee extractEmployeeFromResultSet(ResultSet resultSet) throws SQLException {
+        return new Employee.Builder()
+                .employeeID(resultSet.getInt("employeeID"))
+                .name(resultSet.getString("name"))
+                .birthDate(resultSet.getDate("birthDate").toLocalDate())
+                .workStartTime(resultSet.getTime("workStartTime").toLocalTime())
+                .workEndTime(resultSet.getTime("workEndTime").toLocalTime())
+                .serviceID(resultSet.getInt("serviceID"))
+                .rating(resultSet.getString("rating"))
+                .build();
+    }
 }
